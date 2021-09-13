f8af3c5a2bf1a13b34f7bce52b814e2e5317d043
pinejuice <wata_82_10@yahoo.co.jp>
2021-09-13 21:49:06 +0900

    update index

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,8 @@
 $(function() {
    $('header').load('header.html');
    $('.sidebar').load('sidebar.html');
+   var window_width = document.body.clientWidth;
+   if (window_width <= 500) {
+       
+   }
 });
